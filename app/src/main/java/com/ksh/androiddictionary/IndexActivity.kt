8f3ec0955f808a6580d589f208516f3977b52d9d
ksh <hall7024@naver.com>
2018-06-17 20:55:57 +0900

    no message

---
+++ app/src/main/java/com/ksh/androiddictionary/IndexActivity.kt
@@ -0,0 +1,12 @@
+package com.ksh.androiddictionary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class IndexActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.index_activity)
+    }
+}
